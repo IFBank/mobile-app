948439084b4e9b1deaa34b7f5b9dcfec0b36e83b
exodojaffar <exodojaffar@gmail.com>
2022-01-12 22:20:33 -0400

    update: add tabs faltantes no arquivo

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -2,8 +2,8 @@ import styled from 'styled-components/native';
 
 // TODO: Implementar o estado do input de focus e sua estilizaçao
 export const Container = styled.View`
-border-bottom-width: 2px;
-border-bottom-color: ${ ({focus=false}) => focus ? "#111" : "#C1C1C1" };
+	border-bottom-width: 2px;
+	border-bottom-color: ${ ({focus=false}) => focus ? "#111" : "#C1C1C1" };
 `;
 
 export const InputStyled = styled.TextInput`
