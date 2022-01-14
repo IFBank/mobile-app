8e2d865e4e122b970f0820715a3b147590b537c2
exodojaffar <exodojaffar@gmail.com>
2022-01-13 20:27:02 -0400

    feat: tema dinamico por ThemeContext
    - Tambem coloca o StatusBar como hidde
    
    - Fazer
     - Alguma forma do StatusBar tambem receber o tema e alterar o bgColor

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 import React from "react";
-import { View, StatusBar } from "react-native";
+import { View, StatusBar, useColorScheme } from "react-native";
 
 import { NavigationContainer } from '@react-navigation/native';
+
+import { ThemeContext, themes } from './themes';
 
 import Routes from "./routes"
 
 const App: React.FC  = ( ) => (
 	<NavigationContainer>
-		<StatusBar barStyle="light-content" backgroundColor="#32A041" />
-		<View style={{flex:1, backgroundColor: "#312E36"}}>
-			<Routes />
-		</View>
+
+		<ThemeContext.Provider value={useColorScheme() == "dark"? themes.dark : themes.light}>
+			<StatusBar barStyle="light-content" backgroundColor="#32A041" hidden={true} />
+			<View style={{flex:1, backgroundColor: "#312E36"}}>
+				<Routes />
+			</View>
+		</ThemeContext.Provider>
+			
 	</NavigationContainer>
 );
 
