95dbe088787b29c59b7bdbade2bff2e1977816b9
exodojaffar <exodojaffar@gmail.com>
2022-01-14 16:58:36 -0400

    create: componente ArrowNavegator
    Usado nas telas de cadastro

---
+++ src/components/ArrowNavegator/index.tsx
@@ -0,0 +1,49 @@
+import React, { useContext }from 'react';
+import { ThemeContext } from '../../themes'
+
+import { Platform } from "react-native";
+
+import Icon from "react-native-vector-icons/MaterialIcons"
+
+
+import { Container, IconButtonCircle } from "./styles";
+
+
+interface ArrowNavegatorProps {
+	readonly hasBackScreen?: boolean;
+	backPage?: (any) => void; 
+	nextPage: (any) => void; 
+}
+
+const ArrowNavegator: React.FC<ArrowNavegatorProps> = ({ hasBackScreen, backPage, nextPage, ... rest }) => {
+	const theme = useContext(ThemeContext)
+
+	return (
+		<Container { ... rest } >
+			{/*Left Icon*/}
+			<IconButtonCircle
+				iconStyle={{marginRight: 8}}
+				name={Platform.OS == "ios"? "arrow-back-ios" : "arrow-back" }
+				size={30}
+				backgroundColor={hasBackScreen == true ? theme.primary : theme.secundary_gray} 
+				color="#FFF"
+				borderRadius={50}
+				onPress={ hasBackScreen ? backPage : null}
+			/>
+			
+			{/*Right Icon*/}
+			<IconButtonCircle
+				iconStyle={{marginRight: 8}}
+				name={Platform.OS == "ios"? "arrow-forward-ios" : "arrow-forward" }
+				size={30}
+				backgroundColor={theme.primary}
+				color="#FFF"
+				borderRadius={50}
+				onPress={nextPage}
+			/>
+		</Container>
+	)
+
+};
+
+export default ArrowNavegator;
---
+++ src/components/ArrowNavegator/styles.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components/native';
+
+import Icon from "react-native-vector-icons/MaterialIcons"
+
+export const Container = styled.View`
+	padding: 0 30px;
+	width: 100%;
+	
+	flex-direction: row;
+	justify-content: space-between;
+	align-items: center;
+`;
+
+export const IconButtonCircle = styled(Icon.Button)`
+	width: 50px;
+	height: 50px;
+`;
+
