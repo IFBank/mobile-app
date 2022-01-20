6a72e627be37e833a557099bb49c6969848e8234
exodojaffar <exodojaffar@gmail.com>
2022-01-19 23:16:57 -0400

    feat: tema dado por contexto no aplicativo

--- src/App.tsx
+++ src/App.tsx
@@ -7,18 +7,22 @@ import { ThemeContext, themes } from './themes';
 
 import Routes from "./routes"
 
-const App: React.FC  = ( ) => (
-	<NavigationContainer>
+const App: React.FC  = ( ) => {
+	const theme = useColorScheme() == "dark"? themes.dark : themes.light;
 
-		<ThemeContext.Provider value={useColorScheme() == "dark"? themes.dark : themes.light}>
-			<StatusBar barStyle="light-content" backgroundColor="#32A041" hidden={true} />
-			<View style={{flex:1, backgroundColor: "#312E36"}}>
-				<Routes />
-			</View>
-		</ThemeContext.Provider>
-			
-	</NavigationContainer>
-);
+	return (
+		<NavigationContainer>
+
+			<ThemeContext.Provider value={theme}>
+				<StatusBar barStyle="light-content" backgroundColor={theme.primary} hidden={true} />
+				<View style={{flex:1, backgroundColor: "#312E36"}}>
+					<Routes />
+				</View>
+			</ThemeContext.Provider>
+				
+		</NavigationContainer>
+	)
+};
 
 export default App;
 
