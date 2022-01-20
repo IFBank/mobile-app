de64708eaa8be0aaa940d96f1bfb0eeb2f52d486
exodojaffar <exodojaffar@gmail.com>
2022-01-20 14:46:43 -0400

    feat: navegação entre as telas de cadastro e login

--- src/pages/InitApp/index.tsx
+++ src/pages/InitApp/index.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Image, StyleSheet} from "react-native";
 
+import { useNavigation } from '@react-navigation/native';
+
 import { Container, LogoContainer, ContentContainer, Title, Button, ButtonText } from "./styles"
 
 import logoIFbank from "../../assets/logo.png"
 
 
 const InitApp: React.FC = () => {
+	const navigation = useNavigation();
+
 	return (
 		<Container>
 			<LogoContainer>
@@ -18,11 +22,25 @@ const InitApp: React.FC = () => {
 				Cadastre-se para desfrutar de nosso aplicativo
 				</Title>
 
-				<Button fontSize={20}>
+				<Button 
+					fontSize={20} 
+					onPress={
+						() => {
+							navigation.navigate('CadastroFlow')
+						}	
+					}
+				>
 					Criar cadastro	
 				</Button >
 
-				<Button fontSize={20}>
+				<Button 
+					fontSize={20}
+					onPress={
+						() => {
+							navigation.navigate('Login')
+						}	
+					}
+				>
 					Ja possuo cadastro	
 				</Button>	
 			</ContentContainer>
