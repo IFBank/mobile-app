d8e577bf0f4331e45304e627971b892b8dc819c9
exodojaffar <exodojaffar@gmail.com>
2022-01-20 13:22:11 -0400

    feat: inicia as rotas simples e compostas do aplicativo
    Ha uma constante 'logado' que servirá no futuro para controlar onde o app iniciará, está hard code por agora.
    Rotas compostas ainda não criadas estão com um comentario marcando.
    
    - Todo
     - Status cacheado de logado ou não
     - Rotas compostas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,20 +1,64 @@
 import React from 'react';
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import ScreenExample from '../pages/ScreenExample'
+import ScreenExample from '../pages/ScreenExample';
 
-const Example = createNativeStackNavigator()
+import InitAppScreen from '../pages/InitApp';
+import LoginPageScreen from '../pages/LoginPage';
 
-const ExampleRoutes: React.FC = () => (
-	<Example.Navigator
+import CadastroStageOneScreen from '../pages/SignupFlowPages/StageOne';
+import CadastroStageTwoScreen from '../pages/SignupFlowPages/StageTwo';
+import CadastroStageThreeScreen from '../pages/SignupFlowPages/StageThree';
+import CadastroStagegFourScreen from '../pages/SignupFlowPages/StageFour';
+import CadastroStageFiveScreen from '../pages/SignupFlowPages/StageFive';
+
+const AppStack = createNativeStackNavigator()
+const CadastroFlowStack = createNativeStackNavigator()
+
+const logado = false
+
+const CadastroFlowStackScreen = () => {
+	return (
+		<CadastroFlowStack.Navigator
+			screenOptions={{
+      			headerShown: false,
+      			cardStyle: { backgroundColor: '#312E36' },
+    		}}
+
+    		initialRouteName="StageOne"
+		>
+			<CadastroFlowStack.Screen name="StageOne" component={CadastroStageOneScreen} />
+			<CadastroFlowStack.Screen name="StageTwo" component={CadastroStageTwoScreen} />
+			<CadastroFlowStack.Screen name="StageThree" component={CadastroStageThreeScreen} />
+			<CadastroFlowStack.Screen name="StageFour" component={CadastroStagegFourScreen} />
+			<CadastroFlowStack.Screen name="StageFive" component={CadastroStageFiveScreen} />
+				
+		</CadastroFlowStack.Navigator>
+	)
+}
+
+const AppRoutes: React.FC = () => (
+	<AppStack.Navigator
 		screenOptions={{
       		headerShown: false,
       		cardStyle: { backgroundColor: '#312E36' },
     	}}
+		initialRouteName={logado == true ? "Home" : "InitApp"}
+
 	>
-		<Exemple.Screen name="ScreenExample" component={ScreenExample} />
+		<AppStack.Screen name="InitApp" component={InitAppScreen} />
+		<AppStack.Screen name="Login" component={LoginPageScreen} />
+		<AppStack.Screen name="CadastroFlow" component={CadastroFlowStackScreen} />
+
+		<AppStack.Screen name="Home" component={ScreenExample} />
+		<AppStack.Screen name="Perfil" component={ScreenExample} /> 
+		<AppStack.Screen name="Dashboard" component={ScreenExample} /> 
+		<AppStack.Screen name="Ajuda" component={ScreenExample} /> 
+
+		<AppStack.Screen name="Deposito" component={ScreenExample} /> {/*Será um StackScreen*/}
+		<AppStack.Screen name="Shop" component={ScreenExample} /> {/*Será um StackScreen*/}
 		
-	</Example.Navigator>
+	</AppStack.Navigator>
 );
 
-export default ExampleRoutes;
+export default AppRoutes;
