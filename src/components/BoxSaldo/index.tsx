b5a889d73cf4d11aab1bc3d7682d1760299aed21
exodojaffar <exodojaffar@gmail.com>
2022-01-29 14:42:34 -0400

    feat: componente de pontuação que será muito usado

--- src/components/BoxSaldo/index.tsx
+++ src/components/BoxSaldo/index.tsx
@@ -4,32 +4,16 @@ import { ThemeContext } from '../../themes.ts'
 
 import BoxContainer from '../BoxContainer';
 
-import { SaldoContainer, SaldoText, SaldoTextValue, StyledButton} from "./styles"
+import { LeadingTextStyled, StyledButton} from "./styles"
 
-import DashedLine from 'react-native-dashed-line';
-
-interface BoxSaldoProps {
-
-}
-
-const BoxSaldo: React.FC<BoxSaldoProps> = ({}) => {
+const BoxSaldo: React.FC = () => {
 
 	const theme = useContext(ThemeContext);
 	const value = 12;
 
 	return (
 		<BoxContainer>
-			<SaldoContainer>
-				<SaldoText>
-					Saldo Atual
-				</SaldoText>
-			
-				<DashedLine style={{ flexShrink: 1, marginTop:16, width: "100%"}}/>
-				
-				<SaldoTextValue isLEQZero={value <= 0 }>
-					{`R$ ${value}`}
-				</SaldoTextValue>
-			</SaldoContainer>
+			<LeadingTextStyled fontSize={16} textName="Saldo Atual" textValue={`R$ ${value}`} integerValue={value}/>
 
 			<StyledButton fontSize={12} theme={theme}>
 				Fazer um deposito
