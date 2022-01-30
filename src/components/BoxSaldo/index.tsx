2cdb2ef8fabc0366a11b26b81df62eac0e9daad8
exodojaffar <exodojaffar@gmail.com>
2022-01-30 19:20:46 -0400

    feat: navegação para rota de deposito

--- src/components/BoxSaldo/index.tsx
+++ src/components/BoxSaldo/index.tsx
@@ -1,12 +1,15 @@
-import React, { useContext } from "react";
+import React, { useContext} from "react";
 
 import { ThemeContext } from '../../themes.ts'
+import { useNavigation } from '@react-navigation/native';
 
 import BoxContainer from '../BoxContainer';
 
 import { LeadingTextStyled, StyledButton} from "./styles"
 
 const BoxSaldo: React.FC = () => {
+	const navigation = useNavigation();
+
 
 	const theme = useContext(ThemeContext);
 	const value = 12;
@@ -15,7 +18,15 @@ const BoxSaldo: React.FC = () => {
 		<BoxContainer>
 			<LeadingTextStyled fontSize={16} textName="Saldo Atual" textValue={`R$ ${value}`} integerValue={value}/>
 
-			<StyledButton fontSize={12} theme={theme}>
+			<StyledButton 
+				fontSize={12} 
+				theme={theme} 
+				onPress={ 
+					() => {
+						navigation.navigate('Deposito')
+					}
+				}
+			>
 				Fazer um deposito
 			</StyledButton>
 		</BoxContainer>
