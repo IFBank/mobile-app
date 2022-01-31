e8791c3d496d297a70e5800d3b9e9d36221816a8
exodojaffar <exodojaffar@gmail.com>
2022-01-30 20:03:49 -0400

    feat: implementa o top back button

--- src/components/TopBackButton/index.tsx
+++ src/components/TopBackButton/index.tsx
@@ -1,12 +1,29 @@
 import React from "react";
 
-import {Container} from './styles'
+import { Platform } from "react-native";
 
-const TopBackButton: React.FC = () => {
+import { useNavigation } from '@react-navigation/native';
+
+import { Container, IconStyled } from "./styles";
+
+const TopBackButton: React.FC = ({ ... rest }) => {
+	const navigation = useNavigation();
+
 	return (
-		<Container>
+		<Container { ... rest } >
+
+			<IconStyled
+				iconStyle={{marginRight: 8}}
+				name={Platform.OS == "ios"? "arrow-back-ios" : "arrow-back" }
+				size={30}
+				backgroundColor="#00000000" 
+				color="#000"
+				onPress={ () => {navigation.goBack()}}
+			/>
+			
 		</Container>
 	)
-}
+
+};
 
 export default TopBackButton;
--- src/components/TopBackButton/styles.ts
+++ src/components/TopBackButton/styles.ts
@@ -1,3 +1,16 @@
-import styled from "styled-components/native"
+import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+import Icon from "react-native-vector-icons/MaterialIcons"
+
+export const Container = styled.View`
+	padding: 10px 6px;
+	position: absolute;
+	width: 100%;
+	
+	flex-direction: row;
+	justify-content: flex-start;
+	align-items: center;
+`;
+
+export const IconStyled = styled(Icon)``;
+
