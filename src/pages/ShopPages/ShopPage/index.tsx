7798928be49da937883405f7fefb4985f6fe4443
exodojaffar <exodojaffar@gmail.com>
2022-02-03 16:12:51 -0400

    init: page shop

---
+++ src/pages/ShopPages/ShopPage/index.tsx
@@ -0,0 +1,67 @@
+import React, { useCallback, useState } from 'react';
+
+import { ScrollView } from "react-native";
+
+import { FormHandles } from "@unform/core"
+
+import {Container, MenuContainer, ContentContainer,  FormStyled} from './styles'
+
+import SearchBarInput from "../../../components/SearchBarInput"
+import TabTopShop from "../../../components/TabTopShop"
+import ItemShopBox from "../../../components/ItemShopBox"
+
+const ShopPage = () => {
+	const [ itemType, setItemType ] = useState('salgados')
+
+	const changeItemType = useCallback((value) => {
+		setItemType(value)
+	}, [])
+
+	return (
+		<Container>
+			<MenuContainer>
+
+				<FormStyled>
+					<SearchBarInput name="search"/>
+				</FormStyled>
+
+				<TabTopShop listenChangePage={changeItemType} />				
+
+			</MenuContainer>
+
+			<ScrollView>
+				
+			
+				<ContentContainer>
+					<ItemShopBox 
+						nameItem="Jose" 
+						price="23" 
+						estoqueValue="228" 
+						imageUrl=""
+						onPress={() => {console.log("press")}}
+					/>
+					<ItemShopBox 
+						nameItem="Jose" 
+						price="23" 
+						estoqueValue="228" 
+						imageUrl="" 
+						greenBox
+						onPress={() => {console.log("press")}}
+						style={{marginTop: 20}}
+					/>
+					<ItemShopBox 
+						nameItem="Jose" 
+						price="23" 
+						estoqueValue="228" 
+						imageUrl=""
+						onPress={() => {console.log("press")}}
+						style={{marginTop: 20}}
+
+					/>
+				</ContentContainer>
+			</ScrollView>
+		</Container>
+	);
+}
+
+export default ShopPage;
---
+++ src/pages/ShopPages/ShopPage/styles.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components/native';
+
+import { Form } from "@unform/mobile"
+
+export const Container = styled.View`
+	flex: 1;
+`;
+
+export const MenuContainer = styled.View`
+	width: 100%;
+
+`;
+
+export const ContentContainer = styled.View`
+	flex: 1;
+	padding: 16px 16px;
+`;
+
+export const FormStyled = styled(Form)`
+	padding: 24px 12px;
+	justify-content: center;
+`;
