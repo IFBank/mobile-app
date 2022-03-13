82424ce44912cbc078386863c3a15c59635c8d9e
exodojaffar <exodojaffar@gmail.com>
2022-03-12 22:17:18 -0400

    feat: drop shadow

--- src/components/BoxContainer/index.tsx
+++ src/components/BoxContainer/index.tsx
@@ -1,5 +1,7 @@
 import React, { useContext } from "react";
 
+import DropShadow from "react-native-drop-shadow";
+
 import { ThemeContext } from '../../themes.ts'
 
 import { Container, BorderLinearGradient} from "./styles"
@@ -14,15 +16,24 @@ const BoxContainer: React.FC<BoxContainerProps> = ({children, gradientColor="pri
 
 	const theme = useContext(ThemeContext);
 
-	//TODO: Drop Shadow
-
 	return (
-		<BorderLinearGradient colors={theme.linear[gradientColor]} start={{x: 0, y: 0}} end={{x: 1, y: 0}} style={outerStyle}>
-			<Container theme={theme} { ... rest }>
-				{children}
-			</Container>
-		</BorderLinearGradient>
-
+		<DropShadow
+			style={{
+				shadowColor: theme.shadow,
+				shadowOffset: {
+					width: 0,
+					height: 4,
+				},
+				shadowOpacity: 0.75,
+				shadowRadius: 1,
+			}}
+		>
+			<BorderLinearGradient colors={theme.linear[gradientColor]} start={{x: 0, y: 0}} end={{x: 1, y: 0}} style={outerStyle}>
+				<Container theme={theme} { ... rest }>
+					{children}
+				</Container>
+			</BorderLinearGradient>
+		</DropShadow>
 	)
 }
 
