9711b5cb57c27396ce24cacab83d0f1ce7d57c78
exodojaffar <exodojaffar@gmail.com>
2022-03-13 16:36:08 -0400

    init: black confirm combo page

---
+++ src/pages/ShopPages/ConfirmComboPage/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Container } from "./styles";
+
+const ConfirmComboPage: React.Fc = () => {
+	return (
+		<Container>
+			
+		</Container>
+	)
+}
+
+export default ConfirmComboPage;
