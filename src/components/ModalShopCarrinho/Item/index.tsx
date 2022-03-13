edab94506fb9d40f8134294e0e8946498c5def58
exodojaffar <exodojaffar@gmail.com>
2022-03-13 15:21:59 -0400

    update: new design

--- src/components/ModalShopCarrinho/Item/index.tsx
+++ src/components/ModalShopCarrinho/Item/index.tsx
@@ -1,23 +1,33 @@
-import React from 'react';
+import React, {useContext}from 'react';
 
 import Icon from "react-native-vector-icons/MaterialIcons"
 
-import { ItemContainer, ItemContentContainer, ItemImage, ItemTitle, InfoContainer, InfoName, InfoValue, DeleteButton } from "./styles";
+import { ThemeContext } from '../../../themes'
+
+import { ItemContainer, ItemContentContainer, ItemImage, ItemTitle, InfoContainer, InfoUnid, InfoPrice, DeleteButton } from "./styles";
 
 const Item: React.FC = () => {
+	const theme = useContext(ThemeContext);
+
+	const unidValue = 2;
+	const priceValue = 2.5;
+
 	return(
-		<ItemContainer>
-			<ItemTitle> Teste</ItemTitle>	
+		<ItemContainer
+			outerStyle={{
+				marginBottom: 14
+			}}
+		>
+			<ItemTitle textColor={theme.text.title}> Teste</ItemTitle>	
 
 			<ItemContentContainer>
 				<ItemImage />
 				<InfoContainer>		
-					<InfoName>Quat:<InfoValue> 15</InfoValue>	</InfoName>
-				
-					<InfoName>Quat:<InfoValue> 15</InfoValue>	</InfoName>
+					<InfoUnid textColor={theme.text.title}>{`${unidValue} unid.`}</InfoUnid>
+					<InfoPrice textColor={theme.text.title}>{`R$ ${priceValue}`}</InfoPrice>
 				</InfoContainer>
 
-				<DeleteButton>
+				<DeleteButton colors={theme.linear.semantic_red} start={{x: 0, y: 0}} end={{x: 1, y: 0}}>
 					<Icon  size={40} name="delete" color="#fff"/>
 				</DeleteButton>
 
