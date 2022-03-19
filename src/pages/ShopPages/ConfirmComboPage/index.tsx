8dabd910a1c9066e5964c8acdd252b11a0a9abfe
exodojaffar <exodojaffar@gmail.com>
2022-03-19 12:45:24 -0400

    feat: create combo confirm page

--- src/pages/ShopPages/ConfirmComboPage/index.tsx
+++ src/pages/ShopPages/ConfirmComboPage/index.tsx
@@ -1,11 +1,98 @@
-import React from "react";
+import React, {useRef, useCallback, useContext}from "react";
 
-import { Container } from "./styles";
+import { Form } from "@unform/mobile"
+import { FormHandles } from "@unform/core"
+
+import TopBackButton from '../../../components/TopBackButton';
+import TitleHeader from '../../../components/TitleHeader';
+import BoxContainer from '../../../components/BoxContainer';
+import Input from '../../../components/Input';
+
+import { ThemeContext } from '../../../themes';
+
+import { 
+	Container, 
+	SectionContainer, 
+	UnderLineTitle,
+
+	StyledButton,
+
+	ItemContainer,
+	HeaderItemText,
+	ItemContent,
+	ImageStyled,
+	InfoContainer,
+	RegularText,
+} from "./styles";
+
+const Item: React.FC = ({nameItem, quantItem, imageUrl, theme}) => {
+	return (
+		<ItemContainer gradientColor="secondary" outerStyle={{marginBottom: 14}}>
+			<HeaderItemText textColor={theme.text.title}>
+				{nameItem}
+			</HeaderItemText>
+
+			<ItemContent>
+				<ImageStyled source={imageUrl}/>
+
+				<InfoContainer>
+					<RegularText textColor={theme.text.text}>Quantidade: </RegularText>
+					<RegularText textColor={theme.text.text}> {quantItem} unid.</RegularText>
+				</InfoContainer>
+
+			</ItemContent>
+		</ItemContainer>
+	)
+}
+
+// TODO: Scroll view
+// TODO: Keyboard avoid view
 
 const ConfirmComboPage: React.Fc = () => {
+
+	const theme = useContext(ThemeContext);
+
+	const formRef = useRef<FormHandles>(null)
+
+	const handleSaveCombo = useCallback((data: object) => {
+		// TODO: consulta na API e ações necessarias
+		console.log(data)
+	}, [])
+
 	return (
 		<Container>
+			<TopBackButton />
+			<TitleHeader mainTitle="Montar Combo" subTitle="Confira os produtos e dê um nome ao seu combo." style={{marginTop: 50}}/>
+
+			<SectionContainer>
+				<UnderLineTitle textColor={theme.text.title}>Pedido</UnderLineTitle>			
+
+				<Item nameItem="Pamonha" quantItem={10} theme={theme}/>
+			</SectionContainer>
 			
+			
+
+			<Form ref={formRef} onSubmit={handleSaveCombo}>
+
+				<Input 
+					name="combo-name"
+					headerText="Insira um nome para o combo."
+				/>
+				<StyledButton 
+					text="Criar Combo"
+					textFontSize={24}
+					innerStyle={{
+						paddingVertical: 14
+					}}
+					borderRadius={30}
+					gradientColor="secondary"
+					onPress={
+						() => {
+							formRef.current?.submitForm()
+						}
+					}
+				/>
+			</Form>
 		</Container>
 	)
 }
--- src/pages/ShopPages/ConfirmComboPage/styles.ts
+++ src/pages/ShopPages/ConfirmComboPage/styles.ts
@@ -1,6 +1,71 @@
 import styled from 'styled-components/native';
 
+import { fonts } from '../../../fonts'
+
+import BoxContainer from '../../../components/BoxContainer'
+import GenericButton from '../../../components/Button';
+
 export const Container = styled.View`
 	flex: 1;
 	padding: 0px 16px;
 `;
+
+export const SectionContainer = styled.View`
+	margin-top: 30px;
+`;
+
+export const UnderLineTitle = styled.Text`
+	color: ${ ({textColor}) => textColor};
+
+	width: 100%;
+	text-align: center;
+
+	font-family: ${ fonts.title.bold };
+	font-size: 18px;
+
+	padding: 0 11px;
+	margin-bottom: 6px;
+`;
+
+export const StyledButton = styled(GenericButton)`
+	margin-top: 40px;
+`;
+
+export const ItemContainer = styled(BoxContainer)`
+	padding: 10px 18px;
+`;
+
+export const HeaderItemText = styled.Text`
+	font-family: ${fonts.title.medium};
+	color: ${ ({textColor}) => textColor};
+	font-size: 18px;
+
+	width: 100%;
+	text-align: center;
+`;
+
+export const ItemContent = styled.View`
+	flex-direction: row;
+	align-items: center;
+
+	margin-top: 10px;
+`;
+
+export const ImageStyled = styled.Image`
+	background-color: black;
+	width: 112px;
+	height: 75px;
+	border-radius: 10px;
+
+`
+
+export const InfoContainer = styled.View`
+	margin-left: 12px;
+`;
+
+export const RegularText = styled.Text`
+	font-family: ${fonts.text.regular};
+	color: ${ ({textColor}) => textColor};
+	font-size: 18px;
+`;
+
