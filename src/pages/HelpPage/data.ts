8ce5a56af18e97a1004bdf50bf54eccb830b5350
exodojaffar <exodojaffar@gmail.com>
2022-03-27 12:50:24 -0400

    update: termina a tela de faq do ajuda

---
+++ src/pages/HelpPage/data.ts
@@ -0,0 +1,22 @@
+const asks = [
+	{
+		question:"Posso fazer compras que excedam o meu saldo?",
+		resposta: "Não. O sistema não permitirá a consumação de um pedido possuindo um saldo insuficiente."
+	},
+	{
+		question:"O que acontece se eu não retirar meu pedido até o horário limite?",
+		resposta: "Uma penalidade relativa ao valor do pedido não recebido será aplicada sobre seu próximo pedido."
+	},
+	{
+		question:"Posso reembolsar meu crédito colocado na conta?",
+		resposta: "Ainda não. A equipe de desenvolvimento do IFBank está trabalhando nessa funcionalidade para as próximas versões."
+	},
+	{
+		question:"Posso cancelar meu pedido?",
+		resposta: "Ainda não. A equipe de desenvolvimento do IFBank está trabalhando nessa funcionalidade para as próximas versões."
+	}
+]
+
+const devs = []
+
+export {asks, devs};
