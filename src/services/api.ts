4f03afb6ec5d7fd7093a51ea24d2010eb8d990a2
exodojaffar <exodojaffar@gmail.com>
2022-04-17 17:56:39 -0400

    feat: função pra settar o token de autentica no header
    Tava precisando

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,7 @@
 import axios, { AxiosError } from "axios";
 
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 const BACKEND_URL = "http://localhost:8080";
 
 const apiIFBANK = axios.create({
@@ -7,4 +9,15 @@ const apiIFBANK = axios.create({
     BACKEND_URL,
 });
 
-export { apiIFBANK };
+const setUpAuthToken = async ()  =>{
+  const AUTH_TOKEN = await AsyncStorage.getItem('auth_token');
+
+  if (!AUTH_TOKEN) return;
+
+  apiIFBANK.defaults.headers.common['Authorization'] = AUTH_TOKEN;
+}
+
+export { 
+  apiIFBANK,
+  setUpAuthToken
+};
