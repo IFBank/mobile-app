e5ef34d5423748205dcf5a895652f21b100545d0
exodojaffar <exodojaffar@gmail.com>
2022-04-17 17:57:11 -0400

    feat: setagem do token no inicio do app ou login

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { View, StatusBar, useColorScheme } from "react-native";
 
 import { NavigationContainer } from '@react-navigation/native';
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import { setUpAuthToken } from './services/api';
+import useCacheState from './hooks/useCacheState';
 
 import { ThemeContext, themes } from './themes';
 
@@ -10,6 +15,18 @@ import Routes from "./routes"
 
 const App: React.FC  = ( ) => {
 	const theme = useColorScheme() == "dark"? themes.dark : themes.light;
+
+	/*
+	 Quando o auth_token for mudando em qualquer parte da aplicação
+	 ou quando a aplicação iniciar, será feita a atualização do 
+	 token no axios para o uso na API.
+	*/
+
+	const { state: authToken } = useCacheState('auth_token');
+	
+	useEffect( () => {
+		setUpAuthToken();
+	}, [authToken]);
 
 	return (
 		<NavigationContainer>
